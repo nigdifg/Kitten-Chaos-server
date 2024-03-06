e28c4ff7cb7711d107e408d80b1d6947effa3ded
Anurag <2006535@kiit.ac.in>
2024-03-06 17:49:57 +0530

    Added all backend code

---
+++ src/redisClient.ts
@@ -0,0 +1,26 @@
+import { Client, Repository } from 'redis-om';
+
+import { config } from 'dotenv';
+config()
+
+
+const redisClient = new Client();
+
+
+(async () => {
+    // await redisClient.open(`redis://${process.env.USER}:${process.env.PASS}@${process.env.URL}`)
+    await redisClient.open(`redis://default:gkRneejbeghkp4jIG4zCUyY5grqvJN3L@redis-17324.c10.us-east-1-2.ec2.cloud.redislabs.com:17324`)
+    try {
+        await redisClient.isOpen()
+        console.log('connected to redis')
+    }
+    catch (err) {
+        console.log('error connecting to redis', err)
+    }
+})();
+
+
+
+// redis://${process.env.username}:${process.env.passsword}@${connecctionstrong}
+
+export { redisClient }
